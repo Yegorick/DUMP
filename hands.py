a226986ea31209890d1b8bd56a507bd8e10f806d
Egor <Egoriv27@gmail.com>
2022-10-07 04:04:54 +0300

    Hand to canvas

--- hands.py
+++ hands.py
@@ -1,7 +1,28 @@
-class Hand:
-    def __init__(self, *cards) -> None:
+from tkinter import DISABLED, Canvas, Button, LEFT, DISABLED
+from PIL import ImageTk
+
+class Hand(Canvas):
+    def __init__(self, cards, img=None) -> None:
+        super().__init__()
         self.cards = list(cards)
+        print(self.cards)
+        if not img:
+            self.imgs = [ImageTk.PhotoImage(file=f"imgs/{card.mast}/{card.value}.jpg") for card in self.cards]
+        else:
+            self.imgs = [ImageTk.PhotoImage(file=f"imgs/{img}.jpg") for card in self.cards]
+        self.btns = []
+        for i, img in enumerate(self.imgs):
+            b = Button(self, image=img, command=lambda m=i: self.get_card(m))
+            self.btns.append(b)
         
+        for btn in self.btns:
+            btn.pack(padx=10, pady=10, side=LEFT)
+
+    def get_card(self, i):
+        self.cards[i].move()
+        self.btns[i].destroy()
+
+
     def add_cards(self, *cards):
         for card in cards:
             self.cards.append(card)
@@ -21,6 +42,8 @@ class PCHand(Hand):
     def __init__(self, *cards) -> None:
         super().__init__(*cards)
         self.cards.sort(key=self.sort_by_value)
+        for btn in self.btns:
+            btn["state"] = DISABLED
 
     def sort_by_value(self, card):
         return card.value
--- main.py
+++ main.py
@@ -5,30 +5,33 @@ from hands import *
 from game import new_game
 
 class App(Tk):
-    def __init__(self, cards: list) -> None:
+    def __init__(self, cards: list = None) -> None:
         super().__init__()
-        self.card_list = cards
-        self.imgs = [ImageTk.PhotoImage(file=f"imgs/{card.mast}/{card.value}.jpg") for card in cards]
-        self.btns = []
-        for i, img in enumerate(self.imgs):
-            b = Button(self, image=img, command=lambda m=i: self.get_card(m))
-            self.btns.append(b)
-        
-        for btn in self.btns:
-            btn.pack(padx=10, pady=10, side=LEFT)
-
-    def get_card(self, i):
-        self.card_list[i].move()
-        self.btns[i].destroy()
+    #    self.card_list = cards
+    #    self.imgs = [ImageTk.PhotoImage(file=f"imgs/{card.mast}/{card.value}.jpg") for card in cards]
+    #    self.btns = []
+    #    for i, img in enumerate(self.imgs):
+    #        b = Button(self, image=img, command=lambda m=i: self.get_card(m))
+    #        self.btns.append(b)
+    #    
+    #    for btn in self.btns:
+    #        btn.pack(padx=10, pady=10, side=LEFT)
+#
+    #def get_card(self, i):
+    #    self.card_list[i].move()
+    #    self.btns[i].destroy()
 
 
 
 pl_hand, comp_hand = new_game()
 
-player = Hand(*pl_hand)
-computer = PCHand(*comp_hand)
+window = App()
 
-window = App(player.cards)
+player = Hand(pl_hand)
+computer = PCHand(comp_hand, "back")
+
+computer.pack()
+player.pack()
 
 window.mainloop()
 
